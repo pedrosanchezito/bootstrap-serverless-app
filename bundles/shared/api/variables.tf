f53bcf7200bfe605e31810d3814ccd2faac5ec8f
Julien Jaudou <julienjaudou@gmail.com>
2020-02-09 15:00:23 +0000

    trash script: fix typo, overall cleaning

--- bundles/shared/api/variables.tf
+++ bundles/shared/api/variables.tf
@@ -1,23 +1,15 @@
 variable "aws_region" {
   description = "AWS region where the infrastructure will be deployed"
   type        = string
-  default     = "eu-west-1" # TO BE REMOVED
-}
-
-variable "aws_user" {
-  description = "The AWS user that will deploy the infrastructure"
-  type        = string
-  default     = "generic_user" # TO BE REMOVED
 }
 
 variable "bucket_tfstate_name" {
   description = "S3 bucket where tfstates are stored"
   type        = string
-  default     = "serverless-app-jj-20200128" # TO BE REMOVED
 }
 
 variable "project_name" {
   description = "Name of the project for naming and tagging purpose"
-  type        = "string"
+  type        = string
   default     = "serverless-app"
 }
--- ci_scripts/apply.sh
+++ ci_scripts/apply.sh
@@ -1,52 +1,29 @@
 #!/bin/sh
 
-if [ $CI_MERGE_REQUEST_TARGET_BRANCH_NAME = "master" ]
+if [[ $CI_MERGE_REQUEST_TARGET_BRANCH_NAME = "master" ]]
 then ENV="prod"
-elif [ $CI_MERGE_REQUEST_TARGET_BRANCH_NAME = "staging" ]
+elif [[ $CI_MERGE_REQUEST_TARGET_BRANCH_NAME = "staging" ]]
 then ENV="staging"
 fi
 
-echo $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
-echo $ENV
-
 for shared in $(find /builds/bootstrap-serverless-webapp/deploy/bundles/shared/ -maxdepth 1 -type d)
-do if [ ${shared} != "/builds/bootstrap-serverless-webapp/deploy/bundles/shared/" ]
+do if [[ ${shared} != "/builds/bootstrap-serverless-webapp/deploy/bundles/shared/" ]]
 then
   cd ${shared}
   terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}"
   terraform workspace select $ENV || terraform workspace new $ENV && terraform workspace select $ENV
-  terraform plan -detailed-exitcode
-  if [ ${?} = 1 ]
-  then
-    exit 1
-  elif [ ${?} = 2 ]
-  then
-    terraform apply -auto-approve
-    if [ ${?} != 0 ]
-    then
-      exit 1
-    fi
-  fi
+  terraform apply -auto-approve
+  echo "############# ${shared} completed"
 fi
 done
 
 for feature in $(find /builds/bootstrap-serverless-webapp/deploy/bundles/features/ -maxdepth 1 -type d)
-do if [ ${feature} != "/builds/bootstrap-serverless-webapp/deploy/bundles/features/" ]
+do if [[ ${feature} != "/builds/bootstrap-serverless-webapp/deploy/bundles/features/" ]]
 then
   cd ${feature}
   terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}"
   terraform workspace select $ENV || terraform workspace new $ENV && terraform workspace select $ENV
-  terraform plan -detailed-exitcode
-  if [ ${?} = 1 ]
-  then
-    exit 1
-  elif [ ${?} = 2 ]
-  then
-    terraform apply -auto-approve
-    if [ ${?} != 0 ]
-    then
-      exit 1
-    fi
-  fi
+  terraform apply -auto-approve
+  echo "############# ${feature} completed"
 fi
 done
