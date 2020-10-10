8591cecf9f44e79eb3c709cec7ff519eb20d60ed
pedrozanchezito <ps8560251@gmail.com>
2020-10-10 09:47:29 +0200

    [FIX] gitlab CI and [ADD] validate for testing github actions

--- .ci-scripts/validate.sh
+++ .ci-scripts/validate.sh
@@ -7,18 +7,11 @@ do
   echo "############# VALIDATING ${shared} ..."
   terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $shared
   terraform validate $shared
-  # if [[ ${?} != 0 ]]
-  # then exit 1
-  # fi
 done
 
 for feature in $(find $FOLDERPATH/features/ -mindepth 1 -maxdepth 1 -type d)
 do
-  cd ${feature}
   echo "############# VALIDATING ${feature} ..."
   terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $feature
   terraform validate $feature
-  # if [[ ${?} != 0 ]]
-  # then exit 1
-  # fi
 done
