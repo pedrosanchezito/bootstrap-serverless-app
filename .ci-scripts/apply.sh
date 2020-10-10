45d0e643c69b4c12603674608b96f33fea85c1db
pedrosanchezito <ps8560251@gmail.com>
2020-10-10 10:09:33 +0200

    Feature/GitHub ci (#2)
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI and [ADD] validate for testing github actions
    
    * [FIX] gitlab CI workflow
    
    * [FIX] gitlab CI workflow

--- .ci-scripts/apply.sh
+++ .ci-scripts/apply.sh
@@ -5,7 +5,7 @@ FOLDERPATH=$2
 
 for shared in $(find $FOLDERPATH/shared/ -mindepth 1 -maxdepth 1 -type d)
 do
-  terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $shared
+  # terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $shared
   terraform workspace select $ENV $shared || terraform workspace new $ENV && terraform workspace select $ENV $shared
   terraform apply -auto-approve $shared
   echo "############# ${shared} completed"
@@ -13,7 +13,7 @@ done
 
 for feature in $(find $FOLDERPATH/features/ -mindepth 1 -maxdepth 1 -type d)
 do
-  terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $feature
+  # terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $feature
   terraform workspace select $ENV $feature || terraform workspace new $ENV && terraform workspace select $ENV $feature
   terraform apply -auto-approve $feature
   echo "############# ${feature} completed"
