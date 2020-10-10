0410dbed25af9cb9737145676d8c249a7547055f
pedrozanchezito <ps8560251@gmail.com>
2020-10-10 09:26:06 +0200

    [FIX] gitlab CI and [ADD] validate for testing github actions

--- .ci-scripts/validate.sh
+++ .ci-scripts/validate.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 FOLDERPATH=$1
+REGION=$2
+BUCKET=$3
+TABLE=$4
 
 for shared in $(find $FOLDERPATH/shared/ -mindepth 1 -maxdepth 1 -type d)
 do
   echo "############# VALIDATING ${shared} ..."
-  terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $shared
+  terraform init -backend-config="region=$REGION" -backend-config="bucket=$BUCKET" -backend-config="dynamodb_table=$TABLE" $shared
   terraform validate $shared
   if [[ ${?} != 0 ]]
   then exit 1
@@ -16,7 +19,7 @@ for feature in $(find $FOLDERPATH/features/ -mindepth 1 -maxdepth 1 -type d)
 do
   cd ${feature}
   echo "############# VALIDATING ${feature} ..."
-  terraform init -backend-config="region=${TF_VAR_aws_region}" -backend-config="bucket=${TF_VAR_bucket_tfstate_name}" -backend-config="dynamodb_table=${TF_VAR_dynamodb_tfstate_table}" $feature/
+  terraform init -backend-config="region=$REGION" -backend-config="bucket=$BUCKET" -backend-config="dynamodb_table=$TABLE" $feature/
   terraform validate $feature
   if [[ ${?} != 0 ]]
   then exit 1
